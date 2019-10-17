5a098234f350e9c73f983d695b324bf0e2cd0570
Mu Yang <emfomy@gmail.com>
2019-10-17 11:16:54 +0800

    Update footer.

--- footer.vue
+++ footer.vue
@@ -5,7 +5,7 @@
         <!-- eslint-disable-next-line -->
         Copyright &copy; <b-link href="//ckip.iis.sinica.edu.tw" target="_blank">CKIP Lab</b-link>. All rights reserved.
         <template v-if="designer.length !== 0">
-          {{ '| Design:' }}
+          {{ '| Web Design:' }}
           <template v-for="(item, key) in designer">
             <template v-if="key !== 0">{{ ', ' }}</template>
             <b-link :href="item.href" target="_blank" :key="key">{{ item.name }}</b-link>
